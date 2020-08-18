dc102cbf0c350e4c28c8066f43445f0332fe5508
simon lawrence <deartistsim91@live.com>
2020-08-17 23:38:24 -0500

    Migration table added - Use the command "Update-Database" Add database

---
+++ SchoolWeb/Migrations/20200818041609_AddDatabaseTables.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SchoolWeb.Migrations
+{
+    public partial class AddDatabaseTables : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CreateSchoolVM");
+
+            migrationBuilder.DropTable(
+                name: "RankVM");
+
+            migrationBuilder.DropTable(
+                name: "StudentVM");
+
+            migrationBuilder.DropTable(
+                name: "OrganizationVM");
+
+            migrationBuilder.DropTable(
+                name: "SchoolVM");
+
+            migrationBuilder.DropColumn(
+                name: "Parish",
+                table: "Students");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Parish",
+                table: "Students",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateTable(
+                name: "CreateSchoolVM",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Address = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Parish = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CreateSchoolVM", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "OrganizationVM",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    DateSignedUp = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Location = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    OrgName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PhoneNumber = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UserName = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OrganizationVM", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SchoolVM",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Address = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DateCreated = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Parish = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SchoolVM", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "StudentVM",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Address = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Age = table.Column<int>(type: "int", nullable: false),
+                    DateCreated = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Score = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StudentVM", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "RankVM",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DateCreated = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    OrganizationID = table.Column<string>(type: "nvarchar(450)", nullable: true),
+                    SchoolID = table.Column<int>(type: "int", nullable: false),
+                    Value = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RankVM", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_RankVM_OrganizationVM_OrganizationID",
+                        column: x => x.OrganizationID,
+                        principalTable: "OrganizationVM",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_RankVM_SchoolVM_SchoolID",
+                        column: x => x.SchoolID,
+                        principalTable: "SchoolVM",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RankVM_OrganizationID",
+                table: "RankVM",
+                column: "OrganizationID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RankVM_SchoolID",
+                table: "RankVM",
+                column: "SchoolID");
+        }
+    }
+}
---
+++ SchoolWeb/Migrations/20200818043613_AddNewDatabaseTable21221.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SchoolWeb.Migrations
+{
+    public partial class AddNewDatabaseTable21221 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
